64b1f4f2038e2f079c1028a580bb2c2febd7dc60
philipwmitilinos-max <philipwmitilinos@gmail.com>
2026-01-22 14:40:30 +0100

    Har skapat två packet controller och model. Samt två klasser Product med constuctor, get & set, och ProductsController.
    Har ändrat BigDecimal till flot.
    Fyllt i ProductsController med produkter för att testa att det syns.
    Jag hade problem med denna kod tidigare så det är fel någonstans.

--- src/main/java/se/iths/cecilia/usersandproducts/controller/ProductsController.java
+++ src/main/java/se/iths/cecilia/usersandproducts/controller/ProductsController.java
@@ -1,9 +1,21 @@
 package se.iths.cecilia.usersandproducts.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import se.iths.cecilia.usersandproducts.model.Product;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("/")
 public class ProductsController {
+    @GetMapping
+    public String productList(Model model) {
+        List<Product> products = List.of(
+                new Product("Kläder", 100.0F, "Vit tröja"),
+                new Product("Kläder", 150.0F, "Svarta byxor")
+        );
+        model.addAttribute("product", products);
+        return "product";
+    }
 }
--- src/main/java/se/iths/cecilia/usersandproducts/model/Product.java
+++ src/main/java/se/iths/cecilia/usersandproducts/model/Product.java
@@ -1,18 +1,17 @@
 package se.iths.cecilia.usersandproducts.model;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class Product {
     private BigInteger id;
     private String title;
-    private BigDecimal price;
+    private float price;
     private String name;
 
     public Product() {
     }
 
-    public Product(String title, BigDecimal price, String name) {
+    public Product(String title, float price, String name) {
         this.title = title;
         this.price = price;
         this.name = name;
@@ -34,11 +33,11 @@ public class Product {
         this.title = title;
     }
 
-    public BigDecimal getPrice() {
+    public float getPrice() {
         return price;
     }
 
-    public void setPrice(BigDecimal price) {
+    public void setPrice(float price) {
         this.price = price;
     }
 
