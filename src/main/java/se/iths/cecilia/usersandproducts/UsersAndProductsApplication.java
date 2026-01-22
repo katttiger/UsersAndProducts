387b26eea7524d43791a420cb705be6359a9644d
katttiger <katttiger@gmail.com>
2026-01-22 13:23:02 +0100

    Initial commit

---
+++ src/main/java/se/iths/cecilia/usersandproducts/UsersAndProductsApplication.java
@@ -0,0 +1,13 @@
+package se.iths.cecilia.usersandproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersAndProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersAndProductsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/cecilia/usersandproducts/UsersAndProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.cecilia.usersandproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersAndProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
