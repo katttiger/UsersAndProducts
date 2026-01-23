a9b0e1d0cb7df641efdd7c314dd7eabbec923e46
philipwmitilinos-max <philipwmitilinos@gmail.com>
2026-01-23 13:01:52 +0100

    Har skapat två packet controller och model. Samt två klasser Product med constuctor, get & set, och ProductsController.
    Har ändrat BigDecimal till flot.
    Fyllt i ProductsController med produkter för att testa att det syns.
    Jag hade problem med denna kod tidigare så det är fel någonstans.
    
    Skapat product.html här det något som jag har missat. Löst problemet.
    
    Har flyttat List till service.

--- src/main/java/se/iths/cecilia/usersandproducts/controller/ProductsController.java
+++ src/main/java/se/iths/cecilia/usersandproducts/controller/ProductsController.java
@@ -3,19 +3,19 @@ package se.iths.cecilia.usersandproducts.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import se.iths.cecilia.usersandproducts.model.Product;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.ResponseBody;
+import se.iths.cecilia.usersandproducts.service.ProductsService;
 
 @Controller
 public class ProductsController {
+    private final ProductsService productsService;
+
+    public ProductsController(ProductsService productsService) {
+        this.productsService = productsService;
+    }
+
     @GetMapping("/product")
-    public String productList(Model model) {
-        List<Product> products = List.of(
-                new Product("Kläder", 100.0F, "Vit tröja"),
-                new Product("Kläder", 150.0F, "Svarta byxor")
-        );
-        model.addAttribute("product", products);
-        return "product";
+    public @ResponseBody String productList(Model model) {
+        return productsService.ProductsServerServcie(model);
     }
 }
